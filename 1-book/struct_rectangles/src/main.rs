f47b8cc807a955fd0938c37d3d235edd05121613
Poppy <poppy.sari@rga.com>
2024-05-01 15:59:47 +0400

    ref[3. struct]: move function into implementation's method

--- 1-book/struct_rectangles/src/main.rs
+++ 1-book/struct_rectangles/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect = Rectangle {
         width: 30,
@@ -12,12 +18,9 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect)
+        rect.area()
     );
 
     dbg!(&rect);
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}

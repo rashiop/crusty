ac67e5d5d390ad0daa00c6f4c70058ec0d398e55
Poppy <poppy.sari@rga.com>
2024-05-01 15:47:23 +0400

    ref[3. struct]: use struct

--- 1-book/struct_rectangles/src/main.rs
+++ 1-book/struct_rectangles/src/main.rs
@@ -1,6 +1,14 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
 
-    let rect = (30, 50);
+    let rect: Rectangle {
+        width: 30,
+        height: 50,
+    }
 
     println!(
         "The area of the rectangle is {} square pixels.",
@@ -8,6 +16,6 @@ fn main() {
     );
 }
 
-fn area(dimension: (u32, u32)) -> u32 {
-    dimension.0 * dimension.1
+fn area(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }

d3ab747eded724bd8c374058abb04c8051ca3a62
Poppy <poppy.sari@rga.com>
2024-05-01 15:51:17 +0400

    chore[3. struct]: debug struct

--- 1-book/struct_rectangles/src/main.rs
+++ 1-book/struct_rectangles/src/main.rs
@@ -1,21 +1,23 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-
-    let rect: Rectangle {
+    let rect = Rectangle {
         width: 30,
         height: 50,
-    }
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect)
+        area(&rect)
     );
+
+    dbg!(&rect);
 }
 
-fn area(rectangle: Rectangle) -> u32 {
+fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }

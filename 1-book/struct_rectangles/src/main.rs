377dcb7b4607eb9ded032e934434e3bbeac5ea4b
Poppy <poppy.sari@rga.com>
2024-05-01 16:03:33 +0400

    feat[3. struct]: add method with multiple params

--- 1-book/struct_rectangles/src/main.rs
+++ 1-book/struct_rectangles/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, rect2: &Rectangle) -> bool {
+        self.width > rect2.width && self.height > rect2.height
+    }
 }
 
 fn main() {
@@ -20,6 +24,21 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect.area()
     );
+
+    let rect_small = Rectangle {
+        width: 10,
+        height: 10,
+    };
+
+
+    let rect_large = Rectangle {
+        width: 100,
+        height: 70,
+    };
+
+    println!("Can rect hold rect_small? {}", rect.can_hold(&rect_small));
+    println!("Can rect hold rect_large? {}", rect.can_hold(&rect_large));
+
 
     dbg!(&rect);
 }

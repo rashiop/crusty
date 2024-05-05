ab22e691e07aa4e2ce9fc3344781f3fda5d7e9e7
Poppy <poppy.sari@rga.com>
2024-05-05 15:20:02 +0400

    [package]: use not working inside child module

--- 1-book/packages_crates_module/restaurant/src/lib.rs
+++ 1-book/packages_crates_module/restaurant/src/lib.rs
@@ -6,31 +6,36 @@ pub mod front_of_house {
 
 // bring paths into scope
 // shorter path name
+// hosting now defined in crate root
 use crate::front_of_house::hosting;
 
+// use no longer applies on customer
+// caused error
+
+mod customer {
+
+    pub fn eat_at_restaurant() {
+        // Without use
+        // // Absolute path 
+        // crate::front_of_house::hosting::add_to_waitlist();
+        // // Relative path
+        // front_of_house::hosting::add_to_waitlist();
+
+        // With use
+        hosting::add_to_waitlist();
 
 
-pub fn eat_at_restaurant() {
-    // Without use
-    // // Absolute path 
-    // crate::front_of_house::hosting::add_to_waitlist();
-    // // Relative path
-    // front_of_house::hosting::add_to_waitlist();
+        let mut meal = back_of_house::Breakfast::summer("Whole Grain");
+        // change our mind
+        meal.toast = String::from("Rye");
+        println!("I'd like {} toast please", meal.toast);
 
-    // With use
-    hosting::add_to_waitlist();
+        let order1 = back_of_house::Appetizer::Soup;
+        let order2 = back_of_house::Appetizer::Salad;
+        let order3 = back_of_house::Appetizer::AcaiBowl;
+    }
 
-
-    let mut meal = back_of_house::Breakfast::summer("Whole Grain");
-    // change our mind
-    meal.toast = String::from("Rye");
-    println!("I'd like {} toast please", meal.toast);
-
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
-    let order3 = back_of_house::Appetizer::AcaiBowl;
 }
-
 
 fn deliver_order() {}
 pub mod back_of_house {
